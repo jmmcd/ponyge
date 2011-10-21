a6b6f731f29a43ca4d3b7f12dd50d999af21dbbf
cstuart1021 <cstuart1021@gmail.com>
2011-10-21 07:06:04 +0000

    add __str__ method to tree.py

--- src/tree.py
+++ src/tree.py
@@ -8,8 +8,17 @@ class Tree:
             self.root = expr[0]
             self.children = []
             for child in expr[1:]:
-               self.children.append(Tree(child))
-                    
-            
-
-    
+                if type(child) == tuple:
+                    self.children.append(Tree(child))
+                else:
+                    self.children.append(Tree((child,)))
+    def __str__(self):
+        result = "("
+        result += str(self.root)
+        for child in self.children:
+            if len(child.children) > 0:
+                result += child.__str__()
+            else:
+                result += " " + str(child.root)
+        result += ")"
+        return result
