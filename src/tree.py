218ddc9281860c34f2daa4f7d43011dea8443bce
cstuart1021 <cstuart1021@gmail.com>
2011-12-12 18:29:45 +0000

    added implementation of grammarDerivation and method to call this implementation

--- src/tree.py
+++ src/tree.py
@@ -1,7 +1,7 @@
 import random
+import copy
 arities = {"*":2, "+":2, "-": 2, "1":0, "2":0, "3":0, "4":0, "5":0, "6":0,
            "7":0, "8":0, "9":0, "0":0, "sin":1, "cos":1, "tan":1}
-arities = {"*":4, "+":4, "-": 4, "1":0, "sin":1, "cos":1, "tan":1}
 
 class Tree:
 
@@ -42,6 +42,17 @@ class Tree:
             return self
         else:
             return random.choice(self.children).getRandom(prob)
+
+    def grammarDerivation(self, grammar):
+        productions = grammar.rules[self.root]
+        chosen_prod = random.choice(productions)
+        for i in range(len(chosen_prod)):
+            symbol = chosen_prod[i]
+            if symbol[1] == grammar.T:
+                self.children.append(Tree(symbol[0],self))
+            else:
+                self.children.append(Tree(symbol[0],self))
+                self.children[i].grammarDerivation(grammar)
             
     def grow(self):
         print self.getDepth()
@@ -74,26 +85,100 @@ class Tree:
         return set(labels)
 
 def crossover(tree1, tree2):
-        # equal labels not arities??????
+        tree1 = copy.deepcopy(tree1)
+        tree2 = copy.deepcopy(tree2)
+        
         labels1 = tree1.getLabels()
         labels2 = tree2.getLabels()
         intersection = labels1.intersection(labels2)
 
+        intersection = filter(lambda x: arities[x] != 0, intersection)
+        print intersection
+
+        #see if there is a label to do a crossover
         if len(intersection) != 0:
             t1 = tree1.getRandom(0.2)
-            t2 = tree2.getRandom(0.2)
 
-            while t1.root != t2.root:
+            #finds the nodes to crossover at
+
+            while t1.root not in intersection:
                 t1 = tree1.getRandom(0.2)
+
+            t2 = tree2.getRandom(0.2)
+            while t2.root != t1.root:
                 t2 = tree2.getRandom(0.2)
+                
+            p1 = t1.parent
+            p2 = t2.parent
+            
+            # when the crossover is between the entire tree of both tree1 and tree2
+            if p1 == None and p2 == None:
+                return [t2, t1]
+            #when only t1 is the entire tree1
+            elif p1 == None:
+                tree1 = t2
+                i2 = p2.children.index(t2)
+                p2.children[i2] = t1
+                t1.parent = p2
+            #when only t2 is the entire tree2
+            elif p2 == None:
+                tree2 = t1
+                i1 = p1.children.index(t1)
+                p1.children[i1] = t2
+                t2.parent = p1
+            #when the crossover node for both trees is not the entire tree
+            else:
+                i1 = p1.children.index(t1)
+                i2 = p2.children.index(t2)
 
-            print t1
-            print t2
+                p1.children[i1] = t2
+                p2.children[i2] = t1
 
-            temp1 = t1
-            temp2 = t2
-            t1 = temp2
-            t2 = temp1
+                t2.parent = p1
+                t1.parent = p2
 
         return [tree1, tree2]
 
+def getDerivation(grammar):
+    tree = Tree(grammar.startRule[0], self)
+    tree.grammarDerivation(grammar)
+
+def crossoverTests():
+     tree1 = Tree(("+",("*", "2", "3",),"5"), None)
+     tree2 = Tree(("-","7",("+","1","6")), None)
+
+     #1 common label
+     crossList = crossover(tree1, tree2)
+     assert str(crossList[0]) == "(+ 1 6)"
+     assert str(crossList[1]) == "(- 7 (+ (* 2 3) 5))"
+
+     tree3 = Tree(("-", "7", "1"), None)
+
+     #no common label
+     crossList = crossover(tree1, tree3)
+     assert str(crossList[0]) == str(tree1)
+     assert str(crossList[1]) == str(tree3)
+
+     tree4 = Tree(("-","7",("+","1",("+", "4", "8"))), None)
+
+     #1 common label multiple spots in tree
+     crossList = crossover(tree1, tree4)
+     assert str(crossList[0]) != str(tree1)
+     assert str(crossList[1]) != str(tree4)
+
+     tree5 = Tree(("-","7",("+","1",("+", "3", "5"))), None)
+
+     #multiple common labels
+     crossList = crossover(tree1, tree5)
+     assert str(crossList[0]) != str(tree1)
+     assert str(crossList[1]) != str(tree5)
+
+     tree6 = Tree(("-", "3", "4"), None)
+
+     #common label is a number(arity = 0) ==> do nothing
+     crossList = crossover(tree1, tree6)
+     assert str(crossList[0]) == str(tree1)
+     assert str(crossList[1]) == str(tree6)
+     
+if __name__ == '__main__':
+    crossoverTests()
