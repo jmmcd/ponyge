cf0c921bfdcfda5bd7ae7778917011d849fe18a9
mark.wagy <mark.wagy@gmail.com>
2011-12-29 21:04:57 +0000

    basic psystem steps working. still needs membrane movement

--- src/psystem.py
+++ src/psystem.py
@@ -26,21 +26,34 @@ class Membrane:
         if membranes is not None:
             self.membranes = membranes
         self.string = self.axiom
+        self.done = 0
 
-    def step(self):
-        for membrane in membranes:
-            while i < string_len:
-                for input, output in self.rules:
-                    if self.string[i:i+len(input)] == input:
-                        new_string += output
+    def membranes_step(self):
+        for membrane in self.membranes:
+            membrane.step()
+
+    def rules_step(self):
+        i = 0
+        new_string = ''
+        string_len = len(self.string)
+        while i < string_len:
+            for input, output in self.rules:
+                if self.string[i:i+len(input)] == input:
+                    new_string += output
                     i += len(input)
                     break
-                else:
-                    new_string += self.string[i]
-                    i += 1
-                if self.string == new_string:
-                    self.done = 1
-                self.string = new_string
+            else:
+                new_string += self.string[i]
+                i += 1
+        if self.string == new_string:
+            self.done = 1
+        self.string = new_string
+
+    def step(self):
+        if self.membranes is not None:
+            self.membranes_step()
+        if self.rules is not None:
+            self.rules_step()
 
     def reset(self):
         """Reset the system to its axiom"""
@@ -67,56 +80,47 @@ class Membrane:
         depth_str = ''
         for i in range(0,depth):
             depth_str += ' '
-        str =  depth_str + '%s\n' % self.axiom
+        str =  depth_str + '%s\n' % self.string
         if (self.rules is not None):
             str += '\n'.join([depth_str + '%s -> %s' % x for x in self.rules])
         if (self.membranes is not None):
-            str += '\n'.join(['\n' + depth_str + '{\n%s\n}' 
+            str += '\n'.join([(  depth_str + '{\n' 
+                               + depth_str + '%s\n' 
+                               + depth_str + '}') 
                               % x.__repr__(depth) for x in self.membranes])
         return str
 
 
-class PSystem:
+class PSystem(Membrane):
     """P-System."""
-
-    output_axiom = None
-    membranes = None
-
-    def __init__(self, membranes=None):
+    
+    def __init__(self, axiom, membranes=None):
         """Initialize P-system"""
-        membranes = None
-        if (membranes is not None):
-            self.membranes = membranes
         self.generation = 0
-        self.done = 0
+        Membrane.__init__(self, axiom, None, membranes)
 
     def step(self):
         """Execute one generation of the system."""
         self.generation += 1
-        for membrane in membranes:
-            membrane.step()
+        Membrane.step(self)
+
 
 def main():
     r1 = ('A','AB')
-    r2 = ('B','BA')
+    r2 = ('B','BAC')
     r3 = ('C','ABC')
     a1 = 'A'
     a2 = 'AB'
     m3 = Membrane(a1, [r1,r2])
     m2 = Membrane(a2, [r2,r3], [m3])
     m1 = Membrane(a1, [r1,r2])
-    p1 = PSystem([m1,m2])
+    p = PSystem('A', [m1,m2])
+    while p.generation < 10:
+        print ('GENERATION: ' + str(p.generation) + '\n')
+        print ('OUTPUT: ' + str(p.string) + '\n')
+        print "PSYSTEM: \n"
+        print p.__repr__()
+        p.step()
 
 if __name__ == '__main__': 
     main()
-
-
-r1 = ('A','AB')
-r2 = ('B','BA')
-r3 = ('C','ABC')
-a1 = 'A'
-a2 = 'AB'
-m3 = Membrane(a1, [r1,r2])
-m2 = Membrane(a2, [r2,r3], [m3])
-m1 = Membrane(a1, [r1,r2])
-p1 = PSystem([m1,m2])
